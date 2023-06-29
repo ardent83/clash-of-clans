0dcc183780cb440954fa970c2a8fdbd8a4dbeea7
ardent83 <121909206+ardent83@users.noreply.github.com>
2023-06-29 15:45:30 +0430

    add class Player

---
+++ coc/src/main/java/model/Player.java
@@ -0,0 +1,61 @@
+package model;
+
+import model.Map.Map;
+
+import java.io.Serializable;
+
+public class Player implements Serializable {
+    public Player(String idPlayer, String password, Map map) {
+        this.idPlayer = idPlayer;
+        this.password = password;
+        this.level = 1+((int) (Math.random() * 3.5));
+        this.numberWin = 0;
+        this.numberLose = 0;
+        this.map = map;
+    }
+
+    private final String idPlayer;
+    private final String password;
+
+    public Map getMap() {
+        return map;
+    }
+
+    private final Map map;
+    private int level;
+    private int numberWin;
+    private int numberLose;
+
+    public String getIdPlayer() {
+        return idPlayer;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getNumberWin() {
+        return numberWin;
+    }
+
+    public void setNumberWin(int numberWin) {
+        this.numberWin = numberWin;
+    }
+
+    public int getNumberLose() {
+        return numberLose;
+    }
+
+    public void setNumberLose(int numberLose) {
+        this.numberLose = numberLose;
+    }
+}
+
