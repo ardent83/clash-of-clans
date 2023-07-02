8d24e276f7909af3037909fa626a4a0dc762e8da
ardent83 <121909206+ardent83@users.noreply.github.com>
2023-07-02 21:18:03 +0430

    add new image to Dragon

--- coc/src/main/java/model/hero/Dragon.java
+++ coc/src/main/java/model/hero/Dragon.java
@@ -1,6 +1,10 @@
 package model.hero;
 public class Dragon extends Hero{
     public Dragon(double x, double y) {
-        super(3400, 20, 310, 30, 16, null, 120, 120, x, y, "dragonL.gif");
+        super(3400, 20, 310, 30, 16, null, 120, 120, x, y, "dragonL.gif", "dragon_attackL.gif", "dragonR.gif", "dragon_attackR.gif");
+        getImageViews().get(1).setFitWidth(170);
+        getImageViews().get(1).setFitHeight(120);
+        getImageViews().get(3).setFitWidth(170);
+        getImageViews().get(3).setFitHeight(120);
     }
 }
