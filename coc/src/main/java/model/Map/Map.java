794a34533b276c53496eec00395e89d230a1736f
ardent83 <121909206+ardent83@users.noreply.github.com>
2023-07-02 21:13:21 +0430

    add arraylist from attacking heroes

--- coc/src/main/java/model/Map/Map.java
+++ coc/src/main/java/model/Map/Map.java
@@ -2,18 +2,23 @@ package model.Map;
 
 import javafx.scene.Node;
 import javafx.scene.layout.*;
+import model.hero.Hero;
 
 import java.util.ArrayList;
 
 public abstract class Map {
     public Map(int mapId) {
         this.mapId = mapId;
+        this.attackingHeroes = new ArrayList<>();
     }
-
     private final int mapId;
+    private final ArrayList<Hero> attackingHeroes;
 
     public int getMapId() {
         return mapId;
+    }
+    public ArrayList<Hero> getAttackingHeroes() {
+        return attackingHeroes;
     }
     public abstract AnchorPane getMapView();
     public abstract int getCapacityMap();
